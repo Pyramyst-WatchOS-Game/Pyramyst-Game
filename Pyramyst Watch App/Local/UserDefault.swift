0426a395c5c058f67380bb505fc0803a5d498161
muhamad gatot <gatotbima2002@gmail.com>
2025-07-29 10:48:06 +0700

    update: reources manager assets

--- Pyramyst Watch App/Local/UserDefault.swift
+++ Pyramyst Watch App/Local/UserDefault.swift
@@ -16,9 +16,13 @@ let defaultCollectibleItems: [ItemModel] = [
     ItemModel(name: "Raa's gucci", image: "gucci", isCollected: true, collectedDate: Date(), level: 4),
     ItemModel(name: "Mummy's Firaus", image: "head", isCollected: false, level: 5),
     ItemModel(name: "Free of Ethernity", image: "fire", isCollected: false, level: 6),
-    ItemModel(name: "item7", image: "item4", isCollected: false, level: 7),
-    ItemModel(name: "item8", image: "item1", isCollected: false, level: 8),
-    ItemModel(name: "item9", image: "item0", isCollected: false, level: 9),
-    ItemModel(name: "item10", image: "item3", isCollected: false, level: 10)
+    ItemModel(name: "Golden beetle", image: "bug", isCollected: false, level: 7),
+    ItemModel(name: "Watch of Anubis", image: "compass", isCollected: false, level: 8),
+    ItemModel(name: "Graveyard", image: "stone", isCollected: false, level: 9),
+    ItemModel(name: "Miniature pyramid", image: "pyramid", isCollected: false, level: 10),
+    ItemModel(name: "Egyptian map", image: "map", isCollected: false, level: 11),
+    ItemModel(name: "Ankh necklace", image: "neckless", isCollected: false, level: 12),
+    ItemModel(name: "Papyrus note", image: "paper", isCollected: false, level: 13),
+    ItemModel(name: "Key of Sphinix", image: "key", isCollected: false, level: 14),
 ]
 
