b298008d3584567dbaece01a17cba8804984269d
muhamad gatot <gatotbima2002@gmail.com>
2025-07-29 22:08:39 +0700

    feat: user level

--- Pyramyst Watch App/Local/UserDefault.swift
+++ Pyramyst Watch App/Local/UserDefault.swift
@@ -10,10 +10,10 @@ import Combine
 
 // Dummy init for 10 items collections
 let defaultCollectibleItems: [ItemModel] = [
-    ItemModel(name: "Sandtime Anubis", image: "sand", isCollected: true, level: 1),
-    ItemModel(name: "Car of Anubis", image: "cat", isCollected: true, collectedDate: Calendar.current.date(byAdding: .day, value: -7, to: Date()), level: 2),
-    ItemModel(name: "Desitiny of Egypt", image: "leaf", isCollected: true, collectedDate: Calendar.current.date(byAdding: .day, value: -3, to: Date()), level: 3),
-    ItemModel(name: "Raa's gucci", image: "gucci", isCollected: true, collectedDate: Date(), level: 4),
+    ItemModel(name: "Sandtime Anubis", image: "sand", isCollected: false, level: 1),
+    ItemModel(name: "Cat of Anubis", image: "cat", isCollected: false, collectedDate: Calendar.current.date(byAdding: .day, value: -7, to: Date()), level: 2),
+    ItemModel(name: "Desitiny of Egypt", image: "leaf", isCollected: false, collectedDate: Calendar.current.date(byAdding: .day, value: -3, to: Date()), level: 3),
+    ItemModel(name: "Raa's gucci", image: "gucci", isCollected: false, collectedDate: Date(), level: 4),
     ItemModel(name: "Mummy's Firaus", image: "head", isCollected: false, level: 5),
     ItemModel(name: "Free of Ethernity", image: "fire", isCollected: false, level: 6),
     ItemModel(name: "Golden beetle", image: "bug", isCollected: false, level: 7),
