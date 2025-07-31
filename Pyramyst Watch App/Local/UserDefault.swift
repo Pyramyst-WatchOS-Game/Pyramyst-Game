96f64df6c861a87a6d54af6dc7a7a2fae2a015a6
muhamad gatot <gatotbima2002@gmail.com>
2025-07-31 09:40:21 +0700

    fix: item name

--- Pyramyst Watch App/Local/UserDefault.swift
+++ Pyramyst Watch App/Local/UserDefault.swift
@@ -10,25 +10,25 @@ import Combine
 
 // Dummy init for 10 items collections
 let defaultCollectibleItems: [ItemModel] = [
-    ItemModel(name: "Sandtime Anubis", image: "sand", isCollected: false, level: 1),
-    ItemModel(name: "Cat of Anubis", image: "cat", isCollected: false, collectedDate: Calendar.current.date(byAdding: .day, value: -7, to: Date()), level: 2),
-    ItemModel(name: "Desitiny of Egypt", image: "leaf", isCollected: false, collectedDate: Calendar.current.date(byAdding: .day, value: -3, to: Date()), level: 3),
-    ItemModel(name: "Raa's gucci", image: "gucci", isCollected: false, collectedDate: Date(), level: 4),
-    ItemModel(name: "Mummy's Firaus", image: "head", isCollected: false, level: 5),
-    ItemModel(name: "Free of Ethernity", image: "fire", isCollected: false, level: 6),
-    ItemModel(name: "Golden beetle", image: "bug", isCollected: false, level: 7),
-    ItemModel(name: "Watch of Anubis", image: "compass", isCollected: false, level: 8),
-    ItemModel(name: "Graveyard", image: "stone", isCollected: false, level: 9),
-    ItemModel(name: "Miniature pyramid", image: "pyramid", isCollected: false, level: 10),
-    ItemModel(name: "Egyptian map", image: "map", isCollected: false, level: 11),
-    ItemModel(name: "Ankh necklace", image: "neckless", isCollected: false, level: 12),
-    ItemModel(name: "Papyrus note", image: "paper", isCollected: false, level: 13),
-    ItemModel(name: "Key of Sphinix", image: "key", isCollected: false, level: 14),
-    ItemModel(name: "Ceremonial Cup", image: "mug", isCollected: false, level: 15),
+    ItemModel(name: "The Hourglass", image: "sand", isCollected: false, level: 1),
+    ItemModel(name: "Sacred Cat", image: "cat", isCollected: false, level: 2),
+    ItemModel(name: "The Destiny", image: "leaf", isCollected: false, level: 3),
+    ItemModel(name: "Gucci Relic", image: "gucci", isCollected: false, level: 4),
+    ItemModel(name: "The Mummy", image: "head", isCollected: false, level: 5),
+    ItemModel(name: "Eternal Flame", image: "fire", isCollected: false, level: 6),
+    ItemModel(name: "Desert Bug", image: "bug", isCollected: false, level: 7),
+    ItemModel(name: "The Timepiece", image: "compass", isCollected: false, level: 8),
+    ItemModel(name: "The Tombstone", image: "stone", isCollected: false, level: 9),
+    ItemModel(name: "Mini Pyramid", image: "pyramid", isCollected: false, level: 10),
+    ItemModel(name: "Ancient Map", image: "map", isCollected: false, level: 11),
+    ItemModel(name: "Gold Necklace", image: "neckless", isCollected: false, level: 12),
+    ItemModel(name: "Ancient Note", image: "paper", isCollected: false, level: 13),
+    ItemModel(name: "Ancient Key", image: "key", isCollected: false, level: 14),
+    ItemModel(name: "The Cup of Life", image: "mug", isCollected: false, level: 15),
     ItemModel(name: "Gold Ring", image: "ring", isCollected: false, level: 16),
-    ItemModel(name: "Cobra Idol", image: "cobra", isCollected: false, level: 17),
+    ItemModel(name: "Cobra Statue", image: "cobra", isCollected: false, level: 17),
     ItemModel(name: "Tomb Lock", image: "lock", isCollected: false, level: 18),
-    ItemModel(name: "Ra's Sun Mirror", image: "mirror", isCollected: false, level: 19),
-    ItemModel(name: "Obsidian Dagger", image: "dagger", isCollected: false, level: 20)
+    ItemModel(name: "The Reflection", image: "mirror", isCollected: false, level: 19),
+    ItemModel(name: "Bone Dagger", image: "dagger", isCollected: false, level: 20)
 ]
 
